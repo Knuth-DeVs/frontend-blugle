7332fa847de3bfaec006cab112879f29d18685b2
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-06-23 20:02:27 +0000

    chore: Add routing to render each page

--- src/components/Home/Navbar.tsx
+++ src/components/Home/Navbar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [selectedLink, setSelectedLink] = useState<string>('');
@@ -16,8 +17,13 @@ const Navbar = () => {
                     <img src={logo} alt="Logo" className='w-44' />
                 </div>
                 <ul className="flex gap-8 font-normal text-navlinks text-xl">
+                  <Link to='/'>
                     <li onClick={() => handleLinkClick('Home')} className={`cursor-pointer hover:text-navlinkshover duration-300 ease-linear ${selectedLink === 'Home'? 'underline text-navlinkshover' : ''}`}>Home</li>
+                  </Link>
+                  <Link to='/about'>
                     <li onClick={() => handleLinkClick('About')} className={`cursor-pointer hover:text-navlinkshover duration-300 ease-linear ${selectedLink === 'About'? 'underline text-navlinkshover' : ''}`}>About Us</li>
+                  </Link>
+                    
                     <li onClick={() => handleLinkClick('Company')} className={`cursor-pointer hover:text-navlinkshover duration-300 ease-linear ${selectedLink === 'Company'? 'underline text-navlinkshover' : ''}`}>Contact Us</li>
                     <li onClick={() => handleLinkClick('FAQ')} className={`cursor-pointer hover:text-navlinkshover duration-300 ease-linear ${selectedLink === 'FAQ'? 'underline text-navlinkshover' : ''}`}>FAQ's</li>
                 </ul>
