398a32bc8a14062ea09dfe4acf4da1ad817ba3b3
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-07-13 09:38:34 +0000

    Make changes in the dialog

--- src/components/Home/AlertDialog.tsx
+++ src/components/Home/AlertDialog.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 import { TransitionProps } from '@mui/material/transitions';
 import { Link } from 'react-router-dom';
