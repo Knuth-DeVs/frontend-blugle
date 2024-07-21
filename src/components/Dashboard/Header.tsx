fccab4f57159b01be26d8cf1f270ebe8123ff066
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-07-21 00:30:18 +0000

    feat(Dashboard): Complete the appointment page of the doctor's dashboard

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -1,22 +1,105 @@
-import { Avatar, Badge } from "@mui/material"
+import { Logout, PersonAdd, Settings } from "@mui/icons-material";
+import { Avatar, Badge, Box, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip } from "@mui/material"
+import React from "react";
 import { IoMdSearch } from "react-icons/io"
 import { IoNotifications } from "react-icons/io5"
+import { Link } from "react-router-dom";
 
 const Header = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
     return(
         <div className="border-b-2 w-full flex items-center justify-between my-2 px-2 md:px-5">
             <div className="border rounded-xl px-2 py-2 md:mx-6 mb-2 flex items-center gap-2">
                 <IoMdSearch className="text-2xl text-gray-500"/>
                 <input type="search" placeholder="Type to search" className="outline-none"/>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="flex items-center gap-0">
                 <Badge badgeContent={4} variant="dot" color="error" className="cursor-pointer">
                     <IoNotifications className="text-2xl"/>
                 </Badge>
-                <div className="flex items-center gap-2 cursor-pointer">
-                    <Avatar>H</Avatar>
-                    <p className="font-semibold hidden md:block">Dr. Aric</p>
-                </div>
+                <Box >
+                <Tooltip title="Account settings">
+                    <IconButton
+                        onClick={handleClick}
+                        size="small"
+                        sx={{ ml: 2 }}
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+                    </IconButton>
+                    </Tooltip>
+                </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                    elevation: 0,
+                    sx: {
+                        overflow: 'visible',
+                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                        mt: 1.5,
+                        '& .MuiAvatar-root': {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                        },
+                        '&::before': {
+                        content: '""',
+                        display: 'block',
+                        position: 'absolute',
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: 'background.paper',
+                        transform: 'translateY(-50%) rotate(45deg)',
+                        zIndex: 0,
+                        },
+                    },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Avatar /> Profile
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <PersonAdd fontSize="small" />
+                        </ListItemIcon>
+                        Add another account
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Settings
+                    </MenuItem>
+                    <Link to="/login">
+                        <MenuItem onClick={handleClose}>
+                            <ListItemIcon>
+                                <Logout fontSize="small" />
+                            </ListItemIcon>
+                            Logout
+                        </MenuItem>
+                    </Link>
+                </Menu>
             </div>
         </div>
     )
