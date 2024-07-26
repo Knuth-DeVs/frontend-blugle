e56f4aa987b7edac5af1b1e547ef1c1d39c40a25
Gabby-Tech1 <gkaquainoo@gmail.com>
2024-07-26 00:12:09 +0000

    Fix: fix errors and bugs

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -5,14 +5,10 @@ import logo from "../../assets/logo.png";
 import { BiArrowBack } from "react-icons/bi";
 import { CgSpinner } from 'react-icons/cg';
 
-const LoginForm = () => {
-  const [email, setEmail] = useState('')
-  const [pwd, setPwd] = useState('')
-  const [loading, setLoading] = useState(false)
-
 import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate();
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
@@ -112,13 +108,13 @@ const LoginForm = () => {
               />
             </div>
 
-            {/* <Link to="/doctor/dashboard"> */}
-            <div onClick={()=> setLoading(true)} className='bg-navlinks py-2 px-3 mt-8 rounded-md flex gap-2 items-center justify-center text-center text-white cursor-pointer hover:scale-105 duration-300 ease-linear'>
-            {loading &&
-                  <CgSpinner size={30} className="animate-spin"/>}
-              SIGN IN
+            <div onClick={handleLogin}>
+              <div onClick={()=> setLoading(true)} className='bg-navlinks py-2 px-3 mt-8 rounded-md flex gap-2 items-center justify-center text-center text-white cursor-pointer hover:scale-105 duration-300 ease-linear'>
+              {loading &&
+                    <CgSpinner size={30} className="animate-spin"/>}
+                SIGN IN
+              </div>
             </div>
-            {/* </Link> */}
             </div>
           </form> 
           <p className='pt-3 text-blue-500 cursor-pointer text-right '>Forgot your password?</p>
